0de4152ee33c6ae890eeedef1b9cb9f2f60320ca
Mingyuan Li <mli169@jh.edu>
2023-02-06 13:40:48 -0500

    Add functions for model fitting and output

---
+++ R/hiper_glm.R
@@ -0,0 +1,11 @@
+#' @export
+hiper_glm <- function(design,outcome,model='linear'){
+  support_model <- c('linear','logit')
+  if(!(model %in% support_model))
+    stop(sprintf("Model %s not supported.",model))
+  # TODO: Find maximum likelihood estimate for GLM
+  hglm_out <- list()
+  class(hglm_out) <- 'hglm'
+  warning("hiper_glm not yet implemented.")
+  return(hglm_out)
+}
---
+++ R/model_output.R
@@ -0,0 +1,16 @@
+#' @export
+print.hglm <- function(hglm_out){
+  cat("Output of hiper_glm:\n")
+  # TODO: Display model information
+  warning("print.hglm not yet implemented.")
+}
+#' @export
+coef.hglm <- function(hglm_out){
+  # TODO: Return estimated coefficients
+  warning("coef.hglm not yet implemented.")
+}
+#' @export
+vcov.hglm <- function(hglm_out){
+  # TODO: Return covariance matrix of MLE
+  warning("vcov.hglm not yet implemented.")
+}
