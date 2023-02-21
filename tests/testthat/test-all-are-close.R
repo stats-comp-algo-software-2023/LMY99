961ada26676866bb97be41e456fa9806f715bdfe
Mingyuan Li <mli169@jh.edu>
2023-02-21 01:16:01 -0500

    Add test for helper function

---
+++ tests/testthat/test-all-are-close.R
@@ -0,0 +1,45 @@
+testthat::test_that(
+  "Function returns TRUE when vectors are close",
+  {
+    set.seed(1257)
+    n_vector <- 100
+    # Generate random U(0,1) vectors
+    vec1 <- runif(n_vector,min=1,max=2)
+    # The absolute error is 1e-5
+    # Since abs(vec1) > 1, relative error is at most 1e-5
+    vec2 <- vec1 + 1e-5
+    vec1_vec2_close <- are_all_close(vec1,vec2)
+    testthat::expect_true(
+      are_all_close(vec1,vec2,abs_tol=1e-3,rel_tol=1e-3)
+    )
+})
+testthat::test_that(
+  "Function returns FALSE when absolute error exceeds limit",
+  {
+    set.seed(110)
+    n_vector <- 100
+    # Generate random U(0,1) vectors
+    vec1 <- runif(n_vector,min=1,max=2)
+    # The absolute error is 1e-5
+    # Since abs(vec1) > 1, relative error is at most 1e-5
+    vec2 <- vec1 + 1e-5
+    vec1_vec2_close <- are_all_close(vec1,vec2)
+    testthat::expect_false(
+      are_all_close(vec1,vec2,abs_tol=1e-7,rel_tol=1e-3)
+    )
+})
+testthat::test_that(
+  "Function returns FALSE when relative error exceeds limit",
+  {
+    set.seed(111)
+    n_vector <- 100
+    # Generate random U(0,1) vectors
+    vec1 <- runif(n_vector,min=1,max=2)
+    # The absolute error is 2e-5
+    # Since abs(vec1) < 2, relative error is at least 1e-5
+    vec2 <- vec1 + 2e-5
+    vec1_vec2_close <- are_all_close(vec1,vec2)
+    testthat::expect_false(
+      are_all_close(vec1,vec2,abs_tol=1e-3,rel_tol=1e-7)
+    )
+  })
