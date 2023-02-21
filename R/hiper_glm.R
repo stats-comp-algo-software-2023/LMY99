c0202bd66db5eacfeb4de6f77781b82b58087959
Mingyuan Li <mli169@jh.edu>
2023-02-21 14:15:19 -0500

    Implement MLE for linear model in hiper_glm

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -6,6 +6,18 @@ hiper_glm <- function(design,outcome,model='linear',option=list()){
   # TODO: Find maximum likelihood estimate for GLM
   hglm_out <- list()
   class(hglm_out) <- 'hglm'
-  warning("hiper_glm not yet implemented.")
+  if(model=='linear'){
+    if(is.null(option$mle_solver)){
+      hglm_out$coefficients <- lm_pseudo_inverse(design,outcome)
+      hglm_out$mle_solver <- "PSEUDO_INVERSE"
+    }
+    else if(toupper(option$mle_solver)=='BFGS'){
+      hglm_out$coefficients <- lm_bfgs(design,outcome)
+      hglm_out$mle_solver <- "BFGS"
+    }
+  }
+  else{
+    warning(sprintf("hiper_glm for model %s not yet implemented",model))
+  }
   return(hglm_out)
 }
