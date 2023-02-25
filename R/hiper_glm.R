dd1ece63e76e4e57a8a30c5aa7a1f0e00cfda770
Mingyuan Li <mli169@jh.edu>
2023-02-24 19:29:43 -0500

    Modify condition code for linear model solver

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -7,11 +7,11 @@ hiper_glm <- function(design,outcome,model='linear',option=list()){
   hglm_out <- list()
   class(hglm_out) <- 'hglm'
   if(model=='linear'){
-    if(is.null(option$mle_solver)){
+    if(is.null(option$mle_solver)|option$mle_solver=='PINV'){
       hglm_out$coefficients <- lm_pseudo_inverse(design,outcome)
       hglm_out$mle_solver <- "PSEUDO_INVERSE"
     }
-    else if(toupper(option$mle_solver)=='BFGS'){
+    else if(option$mle_solver=='BFGS'){
       hglm_out$coefficients <- lm_bfgs(design,outcome)
       hglm_out$mle_solver <- "BFGS"
     }
